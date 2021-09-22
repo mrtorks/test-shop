261018a2faf5c52e0ff2227437dcfad53244558c
James Torkornoo <torkornoojames@yahoo.co.uk>
2021-09-21 20:16:17 -0400

    Added favicon

---
+++ src/containers/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed borderless huge menu">
+      <div className="ui container center">
+        <h1>
+          <a href="/">Test Shop</a>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
